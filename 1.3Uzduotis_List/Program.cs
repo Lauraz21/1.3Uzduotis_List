2c76943e8749ff846e6e35d8bd146d54634ba953
Laura <Laura@DESKTOP-BESSB6E>
2023-09-25 22:14:12 +0300

    Add project files.

---
+++ 1.3Uzduotis_List/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _1._3Uzduotis_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 13, 5, 27, 10, 3 };
+            List<int> result = HighterThan10(numbers);
+
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static List<int> HighterThan10(List<int> numbers)
+        {
+            return numbers.FindAll(listNumber => listNumber > 10);
+        }
+    }
+}
+
+
